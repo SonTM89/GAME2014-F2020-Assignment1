a7a7e855d9925f5f0f9f161d3d7ff5145ddcd8fb
Minh Son Tran <tranminhson1989@gmail.com>
2020-10-03 18:31:38 -0400

    Initial Project Scenes

---
+++ Assets/_Scripts/RestartButtonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Event handler for RestartButtonPressed Event
+    public void OnRestartButtonPressed()
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene("GamePlay");
+    }
+}
---
+++ Assets/_Scripts/StartButtonBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Event handler for StartButtonPressed Event
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Hello");
+        SceneManager.LoadScene("GamePlay");
+    }
+}
