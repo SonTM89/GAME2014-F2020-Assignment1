d4686373bf422928f820265596f76a82d540529b
Minh Son Tran <tranminhson1989@gmail.com>
2020-10-04 16:52:45 -0400

    Completing Design for Assignment 1. Can change scenes smoothly.

--- Assets/_Scripts/ReturnButtonBehaviour.cs
+++ Assets/_Scripts/ReturnButtonBehaviour.cs
@@ -1,4 +1,17 @@
-﻿using System.Collections;
+﻿/*--------------------------------------------------------------
+// ReturnButtonBehaviour.cs
+//
+// Handle the event when pressing the MainMenu Button in GameOver Scene.
+//
+// Created by Tran Minh Son on Oct 03 2020
+// StudentID: 101137552
+// Date last Modified: Oct 04 2020
+// Rev: 1.0
+//  
+// Copyright © 2020 Tran Minh Son. All rights reserved.
+--------------------------------------------------------------*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
